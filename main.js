39fc8d9eb4c8ea9150c43894be695ddae4aa2291
angelo-wq <software.developer.angelo@gmail.com>
2025-08-07 19:09:13 -0500

    About avance

--- main.js
+++ main.js
@@ -6,7 +6,41 @@ function closeMenu() {
   app.sheet.close('.menu-modal')
 }
 
-function comprar(producto) {
-  console.log(`Intento de compra del Producto: ${producto}`)
-  window.open(`https://wa.me/51987160920?text=Hola, me gustaría obtener más información del producto *${producto}*, además de saber ¿Cuáles medios de pago están disponibles?`, '_blank')
+function closeShop() {
+  app.sheet.close('.shop-modal')
 }
+
+function comprar(producto) {
+  const inputProduct = document.getElementById('nameProduct');
+  const inputUser = document.getElementById('msgUser');
+  console.log(`Intentando comprar el Producto: ${producto}`);
+  app.sheet.open('.shop-modal');
+  inputProduct.value = producto;
+  inputUser.value = '';
+}
+
+function compraEcha() {
+    const producto = document.getElementById('nameProduct').value;
+    const msg = document.getElementById('msgUser').value;
+    const contact = document.getElementById('contact').value;
+    app.sheet.close('.shop-modal')
+    fetch('https://sheetdb.io/api/v1/1153h8kj2mku3', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data: [
+            {
+                'Producto': producto,
+                'Mensaje': msg,
+                'Contacto': contact
+            }
+        ]
+      })
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+  }
